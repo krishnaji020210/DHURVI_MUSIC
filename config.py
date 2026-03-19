d1c6b35aed56fee3b73b1f78d16f6c7e537256cf
krishnaji020210 <krishnaji020210@gmail.com>
2026-03-19 14:27:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PURVI_SUPPORT")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PURVI_UPDATES")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MOONIEEZ")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+NQGCtLEJJEo3OWY1")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
